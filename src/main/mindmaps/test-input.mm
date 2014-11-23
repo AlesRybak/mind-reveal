0fc7a259c02b5a03835f05ad878b84ab9cfbe3d1
Ales Rybak <ales.rybak@ibacz.eu>
2014-11-23 15:58:13 +0100

    Testing data

---
+++ src/main/mindmaps/test-input.mm
@@ -0,0 +1,126 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1416693589989" ID="ID_654561348" MODIFIED="1416753380382" TEXT="Presentation title">
+<attribute NAME="author" VALUE="AldaR"/>
+<attribute NAME="date" VALUE="bla bla"/>
+<node CREATED="1416693637608" ID="ID_646134934" MODIFIED="1416729705893" POSITION="right" TEXT="Slide 1">
+<icon BUILTIN="list"/>
+</node>
+<node BACKGROUND_COLOR="#ffccff" CREATED="1416693640125" ID="ID_1609240758" MODIFIED="1416748528930" POSITION="right" TEXT="Slide 2">
+<icon BUILTIN="list"/>
+<node CREATED="1416693711828" ID="ID_1722117488" MODIFIED="1416693725106" TEXT="Lorem ipsum dolor sit amet."/>
+<node CREATED="1416693711828" ID="ID_976905834" MODIFIED="1416693725106" TEXT="Lorem ipsum dolor sit amet."/>
+<node CREATED="1416693711828" ID="ID_748486560" MODIFIED="1416693725106" TEXT="Lorem ipsum dolor sit amet."/>
+<node CREATED="1416693711828" ID="ID_1313707679" MODIFIED="1416693725106" TEXT="Lorem ipsum dolor sit amet."/>
+<node CREATED="1416693711828" ID="ID_1460869463" MODIFIED="1416693725106" TEXT="Lorem ipsum dolor sit amet."/>
+</node>
+<node CREATED="1416751336684" ID="ID_328700308" MODIFIED="1416751482086" POSITION="right" TEXT="Two columns slide">
+<icon BUILTIN="list"/>
+<icon BUILTIN="full-2"/>
+<node CREATED="1416751350449" FOLDED="true" ID="ID_972017473" MODIFIED="1416751355728" TEXT="Column one">
+<node CREATED="1416693649885" ID="ID_827186126" MODIFIED="1416693670964" TEXT="Text">
+<node CREATED="1416693711828" ID="ID_990206033" MODIFIED="1416693725106" TEXT="Lorem ipsum dolor sit amet."/>
+<node CREATED="1416693711828" ID="ID_1667571605" MODIFIED="1416693725106" TEXT="Lorem ipsum dolor sit amet."/>
+<node CREATED="1416693711828" ID="ID_342709803" MODIFIED="1416693725106" TEXT="Lorem ipsum dolor sit amet."/>
+</node>
+<node CREATED="1416746025755" ID="ID_850228534" MODIFIED="1416746111397">
+<richcontent TYPE="NODE"><html><head></head><body><img src="images/aaa.png" /></body></html></richcontent>
+</node>
+</node>
+<node CREATED="1416751356203" ID="ID_131765985" MODIFIED="1416751359410" TEXT="Right column">
+<node CREATED="1416693711828" ID="ID_1367862614" MODIFIED="1416737005137" TEXT="Lorem ipsum dolor sit amet.">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#cc00cc" CREATED="1416693711828" ID="ID_553267222" MODIFIED="1416751471146" TEXT="Lorem ipsum dolor sit amet.">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1416693711828" ID="ID_345848525" LINK="#/3/1" MODIFIED="1416735060433" TEXT="Lorem ipsum dolor sit amet."/>
+<node CREATED="1416732351499" ID="ID_239047787" MODIFIED="1416732454908" TEXT="&lt;slide&gt;&#xa;            &lt;h2&gt;&#xa;                &lt;xsl:value-of select=&quot;@TEXT&quot;/&gt;&#xa;            &lt;/h2&gt;&#xa;            &lt;ul&gt;&#xa;                &lt;xsl:for-each select=&quot;node&quot;&gt;&#xa;                    &lt;li&gt;&lt;xsl:value-of select=&quot;@TEXT&quot;/&gt;&lt;/li&gt;&#xa;                &lt;/xsl:for-each&gt;&#xa;            &lt;/ul&gt;&#xa;        &lt;/slide&gt;">
+<icon BUILTIN="pencil"/>
+<attribute NAME="lang" VALUE="xml"/>
+</node>
+</node>
+</node>
+<node CREATED="1416729489638" ID="ID_1677698142" MODIFIED="1416730754901" POSITION="right" TEXT="Section 1">
+<icon BUILTIN="folder"/>
+<node BACKGROUND_COLOR="#ff9966" CREATED="1416693642815" ID="ID_1091705687" MODIFIED="1416748760849" TEXT="Slide 3">
+<icon BUILTIN="list"/>
+<node CREATED="1416693649885" ID="ID_1557201020" MODIFIED="1416693670964" TEXT="Text">
+<node CREATED="1416693711828" ID="ID_1757800848" MODIFIED="1416693725106" TEXT="Lorem ipsum dolor sit amet."/>
+<node CREATED="1416693711828" ID="ID_1654543777" MODIFIED="1416693725106" TEXT="Lorem ipsum dolor sit amet."/>
+<node CREATED="1416693711828" ID="ID_993968942" MODIFIED="1416693725106" TEXT="Lorem ipsum dolor sit amet."/>
+</node>
+<node CREATED="1416693649885" ID="ID_253849611" MODIFIED="1416693670964" TEXT="Text">
+<node CREATED="1416693711828" ID="ID_1613529249" MODIFIED="1416693725106" TEXT="Lorem ipsum dolor sit amet."/>
+<node COLOR="#ff0033" CREATED="1416693711828" ID="ID_872168808" MODIFIED="1416737940788" TEXT="Lorem ipsum dolor sit amet."/>
+<node CREATED="1416693711828" ID="ID_1733722462" MODIFIED="1416737010291" TEXT="Lorem ipsum dolor sit amet.">
+<font BOLD="true" ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1416693649885" ID="ID_1531478821" MODIFIED="1416693670964" TEXT="Text">
+<node CREATED="1416693711828" ID="ID_304060479" MODIFIED="1416693725106" TEXT="Lorem ipsum dolor sit amet."/>
+<node COLOR="#ff0033" CREATED="1416693711828" ID="ID_1449544144" MODIFIED="1416737940788" TEXT="Lorem ipsum dolor sit amet."/>
+<node CREATED="1416693711828" ID="ID_1158754702" MODIFIED="1416737010291" TEXT="Lorem ipsum dolor sit amet.">
+<font BOLD="true" ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1416693649885" ID="ID_1206351409" MODIFIED="1416693670964" TEXT="Text">
+<node CREATED="1416693711828" ID="ID_1068215960" MODIFIED="1416693725106" TEXT="Lorem ipsum dolor sit amet."/>
+<node COLOR="#ff0033" CREATED="1416693711828" ID="ID_570770449" MODIFIED="1416737940788" TEXT="Lorem ipsum dolor sit amet."/>
+<node CREATED="1416693711828" ID="ID_610980235" MODIFIED="1416737010291" TEXT="Lorem ipsum dolor sit amet.">
+<font BOLD="true" ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1416693649885" ID="ID_1066785258" MODIFIED="1416693670964" TEXT="Text">
+<node CREATED="1416693711828" ID="ID_1703333967" MODIFIED="1416693725106" TEXT="Lorem ipsum dolor sit amet."/>
+<node COLOR="#ff0033" CREATED="1416693711828" ID="ID_1626714627" MODIFIED="1416737940788" TEXT="Lorem ipsum dolor sit amet."/>
+<node CREATED="1416693711828" ID="ID_22090282" MODIFIED="1416737010291" TEXT="Lorem ipsum dolor sit amet.">
+<font BOLD="true" ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1416693649885" ID="ID_1245130772" MODIFIED="1416693670964" TEXT="Text">
+<node CREATED="1416693711828" ID="ID_1852817522" MODIFIED="1416693725106" TEXT="Lorem ipsum dolor sit amet."/>
+<node COLOR="#ff0033" CREATED="1416693711828" ID="ID_557906047" MODIFIED="1416737940788" TEXT="Lorem ipsum dolor sit amet."/>
+<node CREATED="1416693711828" ID="ID_872987212" MODIFIED="1416737010291" TEXT="Lorem ipsum dolor sit amet.">
+<font BOLD="true" ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+<node CREATED="1416693642815" ID="ID_351837396" MODIFIED="1416732284784" TEXT="Code slide">
+<icon BUILTIN="list"/>
+<node CREATED="1416693711828" ID="ID_709119033" MODIFIED="1416737005137" TEXT="Lorem ipsum dolor sit amet.">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1416693711828" ID="ID_970443174" MODIFIED="1416737007363" TEXT="Lorem ipsum dolor sit amet.">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1416693711828" ID="ID_1945962710" LINK="#/3/1" MODIFIED="1416735060433" TEXT="Lorem ipsum dolor sit amet."/>
+<node CREATED="1416732351499" HGAP="24" ID="ID_1359768462" MODIFIED="1416753330461" TEXT="&lt;slide&gt;&#xa;            &lt;h2&gt;&#xa;                &lt;xsl:value-of select=&quot;@TEXT&quot;/&gt;&#xa;            &lt;/h2&gt;&#xa;            &lt;ul&gt;&#xa;                &lt;xsl:for-each select=&quot;node&quot;&gt;&#xa;                    &lt;li&gt;&lt;xsl:value-of select=&quot;@TEXT&quot;/&gt;&lt;/li&gt;&#xa;                &lt;/xsl:for-each&gt;&#xa;            &lt;/ul&gt;&#xa;        &lt;/slide&gt;" VSHIFT="-27">
+<icon BUILTIN="pencil"/>
+<attribute NAME="lang" VALUE="xml"/>
+</node>
+</node>
+<node CREATED="1416732304815" FOLDED="true" ID="ID_146978648" MODIFIED="1416734367033" TEXT="Image slide">
+<icon BUILTIN="list"/>
+<node CREATED="1416734387650" ID="ID_182770308" MODIFIED="1416734391133" TEXT="Bla bla"/>
+<node CREATED="1416746025755" ID="ID_1906825568" MODIFIED="1416746111397">
+<richcontent TYPE="NODE"><html><head></head><body><img src="images/aaa.png" /></body></html></richcontent>
+</node>
+<node CREATED="1416734393179" ID="ID_568400262" MODIFIED="1416734395115" TEXT="bla bla"/>
+</node>
+<node CREATED="1416732304815" FOLDED="true" ID="ID_128550487" MODIFIED="1416748211466" TEXT="">
+<icon BUILTIN="list"/>
+<node CREATED="1416746025755" ID="ID_932087387" MODIFIED="1416746111397">
+<richcontent TYPE="NODE"><html><head></head><body><img src="images/aaa.png" /></body></html></richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1416752508291" ID="ID_1226124098" MODIFIED="1416752907247" POSITION="right" TEXT="Linked image slide">
+<icon BUILTIN="list"/>
+<node CREATED="1416752528923" ID="ID_1224508661" LINK="images/aaa.png" MODIFIED="1416753009098" TEXT="Popis obrazku">
+<icon BUILTIN="licq"/>
+</node>
+</node>
+<node CREATED="1416730018291" ID="ID_501320324" MODIFIED="1416730768447" POSITION="right" TEXT="nothing"/>
+</node>
+</map>
